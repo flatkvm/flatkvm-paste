8f4de068b7858566a116a08573348968e9c91006
Sergio Lopez <slp@sinrega.org>
2019-02-05 18:53:11 +0100

    First public release.

---
+++ README.md
@@ -0,0 +1,3 @@
+Flatkvm is a tool to easily run [flatpak](https://flatpak.org/) apps isolated inside a VM, using QEMU/KVM.
+
+flatkvm-paste can be used to send text to a Flatkvm session's clipboard.
---
+++ src/main.rs
@@ -0,0 +1,54 @@
+// flatkvm-paste
+// Copyright (C) 2019  Sergio Lopez <slp@sinrega.org>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+use clap::{crate_authors, crate_version, App, Arg};
+use std::io::{stdin, BufRead, BufReader};
+use x11_clipboard::Clipboard;
+
+fn main() {
+    let cmd_args = App::new("flatkvm-paste")
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about("Paste text into a Flatkvm session.")
+        .arg(
+            Arg::with_name("pid")
+                .required(true)
+                .help("PID of the Flatkvm process managing the session"),
+        )
+        .get_matches();
+
+    let pid: u32 = match cmd_args.value_of("pid") {
+        Some(pid) => pid.parse().unwrap(),
+        None => panic!("invalid arguments"),
+    };
+
+    let mut data = String::new();
+    let reader = BufReader::new(stdin());
+    reader
+        .lines()
+        .map(|d| {
+            data.push_str(&d.unwrap());
+            data.push('\n')
+        })
+        .count();
+
+    let clipboard = Clipboard::new().unwrap();
+    let name = format!("FK{}", pid);
+    let selection = clipboard.setter.get_atom(&name).unwrap();
+    clipboard
+        .store_wait(selection, clipboard.setter.atoms.utf8_string, data)
+        .unwrap();
+}
